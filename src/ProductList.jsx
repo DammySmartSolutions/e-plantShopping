2c7895f17a41c1babac97149887e13bad55c2256
dammysmartsolutions <dammy4edu@gmail.com>
2025-09-28 00:10:02 -0400

    fifth commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import {addItem, removeItem, updateQuantity } from './CartSlice';
+import {addItem } from './CartSlice';
+import { useDispatch } from 'react-redux'; // Add this line
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
@@ -308,7 +309,7 @@ function ProductList({ onHomeClick }) {
           <div className="product-title">{plant.name}</div> {/* Display plant name */}
           {/* Display other plant details like description and cost */}
           <div className="product-description">{plant.description}</div> {/* Display plant description */}
-          <div className="product-cost">${plant.cost}</div> {/* Display plant cost */}
+          <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
           <button
             className="product-button"
             onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
